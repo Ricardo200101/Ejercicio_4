f796d1c225ee78f35ca1a898a12bea731fadafe7
Ricardo Alvarez <SMIS937420@ugb.edu.sv>
2021-07-28 10:25:50 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variable. 
+
+            double gradosc, gradosf, divi, resul;
+
+            // Capturacion de datos. 
+
+            Console.WriteLine("INGRESE LOS GRADOS CENTIGRADOS: ");
+            gradosc = Convert.ToInt32(Console.ReadLine());
+
+            // Operaciones. 
+
+            resul = gradosc * 9;
+            divi = resul / 5;
+
+            gradosf = divi + 32; 
+            Console.WriteLine(" ");
+            Console.WriteLine("LOS GRADOS FAHRENHEIT SON: {0}", gradosf);
+         
+            Console.ReadKey(); 
+        }
+    }
+}
